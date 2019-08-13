0d04877ad3d5e28d39e36c8e659e43462f265e4c
Kappa9 <893387721@qq.com>
2019-08-13 19:42:53 +0800

    git

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance = null;
+	public enum GameState
+	{
+		Title,Selection,Gameplay,Result
+	}
+	public static GameState state = GameState.Title;
+	public bool fading = false;
+
+	public AudioClip[] taikoSound;
+
+	AudioSource au;
+
+	void Awake()
+    {
+		DontDestroyOnLoad(gameObject.transform.parent.gameObject);
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject.transform.parent.gameObject);
+	}
+
+	void OnEnable()
+	{
+		au = GetComponent<AudioSource>();
+	}
+
+	void Update()
+    {
+		Control();
+    }
+
+	void Control()
+	{
+		if (!fading)
+		{
+			if (state == GameState.Selection || state == GameState.Gameplay)
+			{
+				if (Input.GetButtonDown("DonL1") || Input.GetButtonDown("DonR1")) au.PlayOneShot(taikoSound[0]);
+				if (Input.GetButtonDown("KaL1") || Input.GetButtonDown("KaR1")) au.PlayOneShot(taikoSound[1]);
+			}
+			if (Input.GetButtonDown("Start") && state != GameState.Gameplay)
+				au.PlayOneShot(taikoSound[0]);
+		}
+	}
+}
