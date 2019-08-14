b8b3de3a73ac15368a5f400d8b6c2363bf0e89e5
Kappa9 <893387721@qq.com>
2019-08-14 12:00:31 +0800

    读文件

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,9 +12,11 @@ public class GameManager : MonoBehaviour
 		Title,Selection,Gameplay,Result
 	}
 	public static GameState state = GameState.Title;
-	public bool fading = false;
+	public static bool fading = false;
+	public static MusicScore currentSong = null;
 
 	public AudioClip[] taikoSound;
+	public AudioClip selectionMusic;
 
 	AudioSource au;
 
@@ -29,6 +32,7 @@ public class GameManager : MonoBehaviour
 	void OnEnable()
 	{
 		au = GetComponent<AudioSource>();
+		GetMusicList();
 	}
 
 	void Update()
@@ -49,4 +53,47 @@ public class GameManager : MonoBehaviour
 				au.PlayOneShot(taikoSound[0]);
 		}
 	}
+
+	public static List<string> GetMusicList()
+	{
+		string fullPath = "Music/";
+		List<string> filePaths = new List<string>();
+		string filetype = "*.txt|*.tja";
+		string[] FileType = filetype.Split('|');
+		for (int i = 0; i < FileType.Length; i++)
+		{
+			string[] dirs = Directory.GetFiles(fullPath, FileType[i]);
+			for (int j = 0; j < dirs.Length; j++) filePaths.Add(dirs[j]);
+		}
+		return filePaths;
+	}
+
+	public static string[] ReadFile(string filePath)
+	{
+		return File.ReadAllLines(filePath);
+	}
+
+	//public IEnumerator ChangeSong(string name)
+	//{
+	//	//WWW w = new WWW(url_voice);
+	//	//yield return w;
+	//	//myclip = w.audioClip;
+	//	yield return null;
+	//}
+
+}
+public class MusicScore
+{
+	public string title;//标题
+	public string subtitle;//副标题
+	public float bpm;
+	public string wave;//音乐文件名
+	public float offset;
+	public class Course
+	{
+		public int difficulty;//0简单，1普通，2困难，3魔王
+		public int level;//星数
+		public List<string> course = new List<string>();//曲谱
+	}
+	public List<Course> courses = new List<Course>();
 }
---
+++ Assets/Scripts/MusicListManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicListManager : MonoBehaviour
+{
+	GameManager gameManager;
+	List<string> fileList;
+	List<MusicScore> musicList;
+
+    void OnEnable()
+    {
+		if (FindObjectOfType<GameManager>() != null) gameManager = FindObjectOfType<GameManager>();
+		fileList = GameManager.GetMusicList();
+		GetAllMusicInfo();
+		ShowMusicList();
+	}
+
+    void Update()
+    {
+        
+    }
+
+	void ShowMusicList()//显示歌曲列表
+	{
+
+	}
+
+	void ShowLevelList()//选定某歌曲时显示其难度列表
+	{
+
+	}
+
+	void GetAllMusicInfo()
+	{
+		musicList = new List<MusicScore>();
+		foreach (string name in fileList)
+		{
+			MusicScore score = new MusicScore();
+			MusicScore.Course course = new MusicScore.Course();
+			string[] str = GameManager.ReadFile(name);
+			foreach (string i in str)
+			{
+				if (i.StartsWith("TITLE:")) score.title = i.Substring(6);
+				else if (i.StartsWith("SUBTITLE:")) score.subtitle = i.Substring(9);
+				else if (i.StartsWith("BPM:")) score.bpm = float.Parse(i.Substring(4));
+				else if (i.StartsWith("WAVE:")) score.subtitle = i.Substring(5);
+				else if (i.StartsWith("COURSE:")) course.difficulty = int.Parse(i.Substring(7));
+				else if (i.StartsWith("LEVEL:")) course.level = int.Parse(i.Substring(6));
+				else if (i.StartsWith("#END") && course.level > 0)
+				{
+					score.courses.Add(course);
+					course.level = 0;
+				}
+			}
+			if (score.title != "" && score.bpm != 0 && score.courses.Count > 0)
+			{
+				score.courses = score.courses.OrderBy(s => s.difficulty).ToList();
+				musicList.Add(score);
+			}
+		}
+	}
+
+}
